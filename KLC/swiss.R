f19c62e5ba0c44e86197be154e86673acee7555f
ambreenchaudhri <49079761+ambreenchaudhri@users.noreply.github.com>
2019-06-19 11:52:57 -0500

    Update swiss.R

--- KLC/swiss.R
+++ KLC/swiss.R
@@ -3,13 +3,6 @@
 ### Example using KLC   ###
 ############################
 
-
-################# Goals ######################
-# 1. Plot two variables simultaneously
-# 2. Visualize the effects of multiple variables 
-#    simultaneously
-###############################################
-
 # clean workspace
 rm(list=ls())
 
@@ -17,31 +10,46 @@ rm(list=ls())
 #install.packages("datasets")
 library(datasets)
 
-#This is data on swiss fertality rates in the late 1800s.  
+# This is data on swiss fertality rates in the late 1800s.  
 data(swiss)
 
-#recode the Catholic percentage variable to indicate 
+# We want to see whether certain conditions in Swiss provinces 
+# influenced fertility rates:
+#  a.) percentage of men employed in Agriculture
+#  b.) majority Cathlolic vs. Protestant
+
+################# Goals ######################
+# 1. Run a OLS regression on the swiss dataset
+# 2. Visualize the effects of multiple variables 
+#    simultaneously
+###############################################
+
+##################
+# 1. Multiple Linear Regression (with two variables)
+##################
+
+# Recode the Catholic percentage variable to indicate 
 # majority Catholic provinces
 swiss$Catholic <- ifelse(swiss$Catholic > 50, 1, 0)
 
 # Note that we will later assume that provinces that are not
 # majority Catholic are majority Protestant
 
-#look at the data by clicking on the dataset name in your 
+# Look at the data by clicking on the dataset name in your 
 # Workspace/Environment or by using the following command:
 swiss
 
 # We want to understand why some provinces had lower fertility 
-# rates.We have several variables that can explain this: 
+# rates. We have several variables that can explain this: 
 colnames(swiss)
 
 # A simple linear regression looks at the effect of Agriculture
 # on fertility
-mod1<-lm(Fertility~Agriculture, data=swiss)
+mod1 <- lm(Fertility~Agriculture, data=swiss)
 summary(mod1)
 
-# Multiple Regression is simply an extension of linear
-# regression instead of y = a + b*x we now have 
+# Multiple Regression is an extension of linear
+# regression. Instead of y = a + b*x we now have 
 # y = a + b1*x + b2*x. We refer to the things we 
 # estimate (a, b1, and b2) as coefficients. We can 
 # interpret these coefficients exactly like we did 
@@ -52,6 +60,12 @@ summary(mod1)
 # if Agriculture and Catholicism matter simultaneously? 
 mod2 <- lm( Fertility ~ Agriculture + Catholic, swiss)
 summary(mod2)
+
+##################
+# 2. Plotting the Data
+##################
+
+# Now Let's Plot this data:
 
 # create a pdf file
 pdf("swiss.pdf")
